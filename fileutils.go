9a395f7cc36e5b126bc334e06ad23e7b5351b8dc
afeiship <1290657123@qq.com>
2024-10-31 11:33:42 +0800

    feat: add read file

--- fileutils.go
+++ fileutils.go
@@ -2,8 +2,18 @@ package fileutils
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func SayHi() {
 	fmt.Println("Hi from fileutils")
+}
+
+func ReadFile(filename string) []byte {
+	res, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+	return res
 }
