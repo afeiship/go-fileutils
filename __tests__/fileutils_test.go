bb57c8b36b9a08a7418a418b231646b763610879
afeiship <1290657123@qq.com>
2024-10-31 12:36:25 +0800

    feat: add rm rf

--- __tests__/fileutils_test.go
+++ __tests__/fileutils_test.go
@@ -10,3 +10,10 @@ func TestReadFile(f *testing.T) {
 	res := fileutils.ReadFile("../README.md")
 	f.Log(string(res))
 }
+
+func TestRmRf(t *testing.T) {
+	err := fileutils.RmRf("../.tmp")
+	if err != nil {
+		t.Error(err)
+	}
+}
